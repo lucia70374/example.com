dbe2c0cf468a47acee64b0052c6e6f20f13a3742
Tatjana.Vasovic-Bursac <81821646+lucia70374@users.noreply.github.com>
2021-11-06 16:58:55 +0100

    Added files via upload

---
+++ ham.js
@@ -0,0 +1,25 @@
+const menu = document.querySelector(".menu");
+const ham = document.querySelector(".ham");
+const xIcon = document.querySelector(".xIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+ham.addEventListener("click", (toggleMenu) => {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    xIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    xIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+});
+
+const menuLinks = document.querySelectorAll(".menuLink");
+
+menuLinks.forEach (menuLink => {
+    menuLink.addEventListener("click", (toggleMenu) => {
+
+    });
+  
+  });
